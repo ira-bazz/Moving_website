37c7ca7c1c98e6e00493b60d45a9f1b2f908c45e
Irina Ermiychuk <irinaermiychuk@gmail.com>
2023-03-29 07:56:40 +0300

    continue the project

---
+++ form-handler.php
@@ -0,0 +1,31 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$visitor_phone = $_POST['tel'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$date = $_POST['move-day'];
+
+
+$email_from = 'http://carental.fi/muuttopalvelu/';
+
+$email_subject = 'Tarjous Pyyntö';
+
+$email_body = "User Name: $name .\n".
+                "User Email: $visitor_email .\n".
+                "User Phone: $visitor_phone .\n".
+                "Subject: $subject .\n".
+                "User Message: $message .\n";
+                "Desired Moving Day: $date .\n";
+
+$to = 'movingservicefinland@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
